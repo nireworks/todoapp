5fa98d55c530da5c2465897193d0e9e56a999ec9
Daniel Javorszky <daniel.javorszky@gmail.com>
2019-04-19 22:03:41 +0200

    Add minimal tests

--- store/store_test.go
+++ store/store_test.go
@@ -1,7 +1,6 @@
 package store
 
 import (
-	"reflect"
 	"testing"
 	"todoapp/model"
 
@@ -9,93 +8,88 @@ import (
 )
 
 func TestInMemoryStore_Add(t *testing.T) {
-	type args struct {
-		in0 *model.Todo
-	}
 	tests := []struct {
 		name    string
-		ims     *InMemoryStore
-		args    args
+		todo    *model.Todo
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			"make it successful",
+			&model.Todo{
+				Id:        1,
+				Title:     "Say hello",
+				Completed: false,
+			},
+			false,
+		},
 	}
 	for _, tt := range tests {
+		ims := NewInMemoryStore()
+
 		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.ims.Add(tt.args.in0); (err != nil) != tt.wantErr {
+			if err := ims.Add(tt.todo); (err != nil) != tt.wantErr {
 				t.Errorf("InMemoryStore.Add() error = %v, wantErr %v", err, tt.wantErr)
+				return
 			}
+
+			if tt.wantErr {
+				return
+			}
+
+			readTodo, err := ims.GetById(tt.todo.Id)
+			if assert.NoError(t, err) {
+				assert.Equal(t, tt.todo, readTodo)
+			}
+
 		})
 	}
 }
 
 func TestInMemoryStore_GetById(t *testing.T) {
-	type args struct {
-		in0 int
-	}
 	tests := []struct {
 		name    string
-		ims     *InMemoryStore
-		args    args
-		want    *model.Todo
+		todo    *model.Todo
+		byId    int
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			"make it successful",
+			&model.Todo{
+				Id:        1,
+				Title:     "Say hello",
+				Completed: false,
+			},
+			1,
+			false,
+		},
+		{
+			"make it fail",
+			&model.Todo{
+				Id:        1,
+				Title:     "Say hello",
+				Completed: false,
+			},
+			2,
+			true,
+		},
 	}
 	for _, tt := range tests {
+		ims := NewInMemoryStore()
+
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.ims.GetById(tt.args.in0)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("InMemoryStore.GetById() error = %v, wantErr %v", err, tt.wantErr)
+			err := ims.Add(tt.todo)
+			if assert.Error(t, err) {
+				t.Fail()
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("InMemoryStore.GetById() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
-func TestInMemoryStore_GetAll(t *testing.T) {
-	tests := []struct {
-		name    string
-		ims     *InMemoryStore
-		want    *[]model.Todo
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.ims.GetAll()
+			readTodo, err := ims.GetById(tt.byId)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("InMemoryStore.GetAll() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("GetById(%d) wantErr=%t, but got: %v", tt.byId, tt.wantErr, err)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("InMemoryStore.GetAll() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
-func TestInMemoryStore_Delete(t *testing.T) {
-	type args struct {
-		in0 *model.Todo
-	}
-	tests := []struct {
-		name    string
-		ims     *InMemoryStore
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.ims.Delete(tt.args.in0); (err != nil) != tt.wantErr {
-				t.Errorf("InMemoryStore.Delete() error = %v, wantErr %v", err, tt.wantErr)
-			}
+			assert.Equal(t, tt.todo, readTodo)
 		})
 	}
 }
