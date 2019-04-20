17df2ea6cd606db76a3aa39f9df03c26e7bbe898
Daniel Javorszky <daniel.javorszky@gmail.com>
2019-04-20 16:56:34 +0200

    Add server test

--- cmd/server/server_test.go
+++ cmd/server/server_test.go
@@ -4,18 +4,56 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"todoapp"
+	"todoapp/cmd/server"
+	"todoapp/model"
+	"todoapp/store"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestHandleAddTodo(t *testing.T) {
-	srv := server.server{
-		db:    mockDatabase,
-		email: mockEmailSender,
+func TestHandle_GetTodos(t *testing.T) {
+	tests := []struct {
+		name       string
+		todos      []*model.Todo
+		wantBody   string
+		wantStatus int
+	}{
+		{
+			name:       "empty store",
+			todos:      []*model.Todo{},
+			wantBody:   "[]",
+			wantStatus: http.StatusOK,
+		},
+		{
+			name: "one todo",
+			todos: []*model.Todo{
+				{Title: "Hey"},
+			},
+			wantBody:   "[{\"id\":1,\"title\":\"Hey\",\"completed\":false}]",
+			wantStatus: http.StatusOK,
+		},
 	}
-	srv.routes()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockStore := store.NewInMemoryStore()
 
-	req, err := http.NewRequest("GET", "/about", nil)
-	is.NoErr(err)
-	w := httptest.NewRecorder()
-	srv.ServeHTTP(w, req)
-	is.Equal(w.StatusCode, http.StatusOK)
+			for _, todo := range tt.todos {
+				err := mockStore.Add(todo)
+				assert.NoError(t, err)
+			}
+
+			srv := server.New(todoapp.New(mockStore))
+
+			req, err := http.NewRequest("GET", "/v0/todos", nil)
+			assert.NoError(t, err)
+
+			w := httptest.NewRecorder()
+			srv.ServeHTTP(w, req)
+
+			assert.Equal(t, tt.wantStatus, w.Code)
+			assert.Equal(t, tt.wantBody, w.Body.String())
+
+		})
+	}
 }
