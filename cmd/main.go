d92768e69b0df9ac2014edf6870dda11646a3488
Daniel Javorszky <daniel.javorszky@gmail.com>
2019-04-20 14:27:57 +0200

    Implement a server

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,57 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+	"todoapp"
+	"todoapp/store"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	contentTypeKey  = "Content-Type"
+	applicationJSON = "application/json"
+)
+
+var todoService = todoapp.New(store.NewInMemoryStore())
 
 func main() {
-	log.Println("works")
+	r := mux.NewRouter()
+	r.HandleFunc("/v0/todos", getTodos)
 
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
 
+	log.Fatal(srv.ListenAndServe())
+}
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	todos, err := todoService.GetTodos()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed fetching todos: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	resp, err := json.Marshal(todos)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed marshalling response: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set(contentTypeKey, applicationJSON)
+	w.WriteHeader(http.StatusOK)
+
+	_, err = w.Write(resp)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed writing response: %v", err), http.StatusInternalServerError)
+		return
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module todoapp
 
 go 1.12
 
-require github.com/stretchr/testify v1.3.0
+require (
+	github.com/gorilla/mux v1.7.1
+	github.com/stretchr/testify v1.3.0
+)
