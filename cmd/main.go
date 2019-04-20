cd90d91af54b0a20dbd7a47080ed305c947076ea
Daniel Javorszky <daniel.javorszky@gmail.com>
2019-04-20 14:37:19 +0200

    Implement listing todos and adding them

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"time"
 	"todoapp"
+	"todoapp/model"
 	"todoapp/store"
 
 	"github.com/gorilla/mux"
@@ -21,7 +22,9 @@ var todoService = todoapp.New(store.NewInMemoryStore())
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/v0/todos", getTodos)
+	r.HandleFunc("/v0/todos", getTodos).Methods(http.MethodGet)
+	r.HandleFunc("/v0/todos", addTodo).Methods(http.MethodPost)
+	r.HandleFunc("/v0/todos", updateTodo).Methods(http.MethodPut)
 
 	srv := &http.Server{
 		Handler:      r,
@@ -54,4 +57,33 @@ func getTodos(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, fmt.Sprintf("failed writing response: %v", err), http.StatusInternalServerError)
 		return
 	}
+}
+
+func addTodo(w http.ResponseWriter, r *http.Request) {
+	var todo model.Todo
+
+	err := json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed decoding request: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	err = todoService.SaveTodo(&todo)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed saving todo: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set(contentTypeKey, applicationJSON)
+	w.WriteHeader(http.StatusOK)
+
+	_, err = w.Write([]byte("Success!"))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed writing response: %v", err), http.StatusInternalServerError)
+		return
+	}
+}
+
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+
 }
